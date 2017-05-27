2f9fcc24fcc026ad77c5f22c234ee45b0515be7c
Pascal Zingg <pazingg@gmail.com>
2017-05-27 10:38:09 +0200

    Label visualisation

--- gather_delays.py
+++ gather_delays.py
@@ -2,9 +2,11 @@ import requests
 import json
 import pdb
 import sys
+import math
 
 from qgis.core import *
 from qgis.gui import *
+from osgeo import ogr
 from PyQt4 import *
 from PyQt4 import *
 from PyQt4.QtCore import *
@@ -57,6 +59,53 @@ def get_results_by_town(town_name):
             results[dict_key]['cnt'] += 1
 
 
+def init_ch_layer():
+    layer = QgsVectorLayer(
+        "ADMGDE_GDEDAT.shp", "Indicatrix", "ogr")
+    crs = layer.crs()
+    crs.createFromId(2056)
+    layer.setCrs(crs)
+    if not layer.isValid():
+        print "Failed to open the layer"
+
+    symbols = layer.rendererV2().symbols()
+    symbol = symbols[0]
+    symbol.setColor(QColor.fromRgb(0, 153, 255))
+    return layer
+
+
+def init_features_layer():
+    # create a memory layer with points
+    layer = QgsVectorLayer('Point', 'points', "memory")
+    layer.dataProvider().addAttributes(
+        [QgsField("city",  QVariant.String), QgsField("lbl", QVariant.String)])
+    layer.updateFields()
+
+    symbols = layer.rendererV2().symbols()
+    symbol = symbols[0]
+    symbol.setColor(QColor.fromRgb(0, 0, 0))
+
+    for city in results:
+        long_coord = results[city]['geopos'][1]
+        lat_coord = results[city]['geopos'][0]
+
+        feature = QgsFeature()
+        point = QgsPoint(long_coord, lat_coord)
+        pointGeo = QgsGeometry.fromPoint(point)
+        tr = QgsCoordinateTransform(
+            QgsCoordinateReferenceSystem(4326),
+            QgsCoordinateReferenceSystem(2056)
+        )
+        pointGeo.transform(tr)
+        feature.setGeometry(pointGeo)
+        feat_lbl = 'Delays: ' + str(results[city]['cnt'])
+        feature.setAttributes([city, feat_lbl])
+
+        layer.dataProvider().addFeatures([feature])
+        layer.updateExtents()
+    return layer
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) != 2:
@@ -65,8 +114,8 @@ if __name__ == '__main__':
 
     api_key = sys.argv[1]
 
-    # for city in cities:
-    #     get_results_by_town(city)
+    for city in cities:
+        get_results_by_town(city)
 
     # print json.dumps(results, indent=4)
 
@@ -74,19 +123,34 @@ if __name__ == '__main__':
     QgsApplication.setPrefixPath('/usr', True)
     QgsApplication.initQgis()
 
+    ch_layer = init_ch_layer()
+    features_layer = init_features_layer()
+
+    QgsMapLayerRegistry.instance().addMapLayer(features_layer)
+    QgsMapLayerRegistry.instance().addMapLayer(ch_layer)
+
+    palyr = QgsPalLayerSettings()
+    palyr.readFromLayer(features_layer)
+    palyr.enabled = True
+    palyr.fieldName = 'lbl'
+    palyr.placement = QgsPalLayerSettings.OverPoint
+    palyr.fontSizeInMapUnits = False
+    palyr.textColor = QColor(0, 0, 0)
+    palyr.yOffset = -3
+    palyr.setDataDefinedProperty(QgsPalLayerSettings.OffsetUnits, True, True, '1', '')
+    palyr.setDataDefinedProperty(QgsPalLayerSettings.Bold, True, True, '1', '')
+    palyr.setDataDefinedProperty(QgsPalLayerSettings.Size, True, True, '9', '')
+    palyr.writeToLayer(features_layer)
+
     canvas = QgsMapCanvas()
     canvas.setCanvasColor(Qt.white)
     canvas.enableAntiAliasing(True)
+    canvas.setExtent(ch_layer.extent())
+    canvas.mapRenderer().setLabelingEngine(QgsPalLabeling())
+    canvas.setLayerSet([
+        QgsMapCanvasLayer(features_layer),
+        QgsMapCanvasLayer(ch_layer)
+    ])
     canvas.show()
 
-    layer = QgsVectorLayer(
-        "ADMGDE_GDEDAT.shp", "Indicatrix", "ogr")
-    if not layer.isValid():
-        print "Failed to open the layer"
-
-    QgsMapLayerRegistry.instance().addMapLayer(layer)
-    canvas.setExtent(layer.extent())
-    canvas.setLayerSet([QgsMapCanvasLayer(layer)])
-
-    app.exec_()
-    QgsApplication.exitQgis()
+    sys.exit(app.exec_())
