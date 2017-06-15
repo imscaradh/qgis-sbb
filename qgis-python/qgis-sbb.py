56d51e42d3b612888e967b3950c87ca53ecdca0f
Pascal Zingg <pazingg@gmail.com>
2017-06-15 09:56:36 +0200

    Doc

--- qgis-python/qgis-sbb.py
+++ qgis-python/qgis-sbb.py
@@ -30,6 +30,7 @@ lbl_name = 'lbl'
 
 
 def get_results_by_town(town_name):
+    """Request JSON from SBB-API by bhf name"""
     params = {
         'apikey': api_key,
         'dataset': 'ist-daten-sbb',
@@ -42,6 +43,7 @@ def get_results_by_town(town_name):
 
     records = data['records']
 
+    # Rearrange JSON file for further processing
     for record in records:
         fields = record['fields']
 
@@ -65,6 +67,7 @@ def get_results_by_town(town_name):
 
 
 def init_ch_layer():
+    """Initialize CH-Canton borders from shapefile"""
     # Shapefile from
     # http://www.arcgis.com/home/item.html?id=a5067fb3b0b74b188d7b650fa5c64b39
     layer = QgsVectorLayer("Kantone.shp", "Kantone", "ogr")
@@ -82,6 +85,7 @@ def init_ch_layer():
 
 
 def init_features_layer():
+    """Create layer with outages and delays as features"""
     # create a memory layer with points
     layer = QgsVectorLayer('Point', 'points', "memory")
     layer.dataProvider().addAttributes([QgsField(lbl_name, QVariant.String)])
@@ -91,6 +95,7 @@ def init_features_layer():
     symbol = symbols[0]
     symbol.setColor(QColor.fromRgb(0, 0, 0))
 
+    # One feature contains the geopos, no. of outages and delays
     for city in results:
         long_coord = results[city]['geopos'][1]
         lat_coord = results[city]['geopos'][0]
@@ -104,7 +109,9 @@ def init_features_layer():
         )
         pointGeo.transform(tr)
         feature.setGeometry(pointGeo)
-        feat_lbl = city + '\n\nDelays: ' + str(results[city]['delays']) + '\nOutages:' + str(results[city]['outages'])
+        feat_lbl = city + '\n\nDelays: ' + \
+            str(results[city]['delays']) + '\nOutages:' + \
+            str(results[city]['outages'])
         feature.setAttributes([feat_lbl])
 
         layer.dataProvider().addFeatures([feature])
@@ -113,6 +120,7 @@ def init_features_layer():
 
 
 def configure_labels(layer, field_name):
+    """Setup labels for features"""
     palyr = QgsPalLayerSettings()
     palyr.readFromLayer(layer)
     palyr.enabled = True
@@ -135,22 +143,27 @@ if __name__ == '__main__':
     else:
         api_key = sys.argv[1]
 
+    # Call API to get outages and delays by town
     for city in cities:
         get_results_by_town(city)
 
     print json.dumps(results, indent=4)
 
+    # Startup QGIS
     app = QgsApplication([], True)
     QgsApplication.setPrefixPath('/usr', True)
     QgsApplication.initQgis()
 
+    # Init layers
     ch_layer = init_ch_layer()
     features_layer = init_features_layer()
     configure_labels(features_layer, lbl_name)
 
+    # Register layers
     QgsMapLayerRegistry.instance().addMapLayer(features_layer)
     QgsMapLayerRegistry.instance().addMapLayer(ch_layer)
 
+    # Setup and show canvas
     canvas = QgsMapCanvas()
     canvas.setCanvasColor(Qt.white)
     canvas.enableAntiAliasing(True)
